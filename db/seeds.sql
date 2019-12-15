4f71903178a36ac997cf1308ad6f91aa93f4599c
Levi Giraud <giraudlevi@gmail.com>
2019-12-15 15:24:30 -0700

    added files to start building on

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers  (name) VALUES ('Plain Burger');
+INSERT INTO burgers  (name) VALUES ('Buffalo Chicken Burger');
+INSERT INTO burgers  (name) VALUES ('Deluxe Burger');
+INSERT INTO burgers  (name) VALUES ('BBQ Burger');
+INSERT INTO burgers  (name) VALUES ('Veggie Burger');
